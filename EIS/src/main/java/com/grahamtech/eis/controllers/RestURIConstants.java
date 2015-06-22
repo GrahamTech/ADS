7284089005e6a9e31eb603aa2031e0dc9bdd67dc
rodney4299 <rodney4299@yahoo.com>
2015-06-22 00:32:53 -0400

    Change-Id: Ia2a2b3ca078165cc77d680cb4c33d63a6d1c407b Added CRUD operations for Adverse Drug Events

--- EIS/src/main/java/com/grahamtech/eis/controllers/RestURIConstants.java
+++ EIS/src/main/java/com/grahamtech/eis/controllers/RestURIConstants.java
@@ -2,178 +2,20 @@ package com.grahamtech.eis.controllers;
 
 public class RestURIConstants {
 
-  // BaseController - User Profile
-  /*
-   * No URI Method Details 1 /users GET To get the list of all the Employees in
-   * the data store 2 /user/{id} GET To get the Employee object based on the id
-   * 3 /user/create POST To create the Employee object and store it 4
-   * /user/update POST To update the Employee object in the store 4
-   * /user/delete/{id} PUT To delete the Employee object from the data storage
-   * based on the id
-   */
-    // public static final String GET_DRUG_EVENTS =
-    // "/get/drug/events/{rowLimit}";
+    public static final String CONTROLLER_PREFIX = "/gt";
     public static final String GET_DRUG_EVENTS_CALL_WITH_API_KEY = "/get/drug/events/apikey/{rowLimit}";
     public static final String GET_DRUG_EVENTS_AND_STORE_CALL_WITH_API_KEY = "/get/drug/events/and/store/apikey/{rowLimit}";
-
-    // public static final String GET_DRUG_EVENTS_STORE_AND_DISPLAY_LIST =
-    // "/get/drug/events/store/list";
-    // public static final String GET_DRUG_EVENTS_STORE_AND_DISPLAY_ARRAY =
-    // "/get/drug/events/store/array";
   public static final String ADVERSE_DRUG_EVENT_REPORTS_EXTERNAL_URL =
       "https://api.fda.gov/drug/event.json";
     public static final String ADVERSE_DRUG_EVENT_REPORTS_API_KEY_HEADER = "api_key";
   public static final String ADVERSE_DRUG_EVENT_REPORTS_API_KEY =
       "YQG9X0JzsVSlL7WE8vx9KNiXM3jpyZz1jbhqpTGu";
-  
-    public static final String GET_DRUG_EVENTS_FROM_DATA_STORE = "/get/drug/events/from/data/store";
 
-    // ////////////////////////////
+    public static final String CREATE_DB_EVENT = "/create/db/event";
+    public static final String READ_DB_EVENTS = "/read/db/events";
+    public static final String READ_DB_EVENT_BY_ID = "/read/db/event/{id}";
+    public static final String UPDATE_DB_EVENT_BY_ID = "/update/db/event/{id}";
+    public static final String DELETE_DB_EVENT_BY_ID = "/delete/db/event/{id}";
 
-  public static final String GET_PROFILES = "/get/profiles";
-  public static final String GET_PROFILE = "/get/profile/{id}";
-  public static final String UPDATE_PROFILE = "/update/profile/{id}";
-  public static final String DELETE_PROFILE = "/delete/profile/{id}";
-  public static final String CREATE_PROFILE = "/create/profile";
-
-  public static final String GET_USERS = "/get/users";
-  public static final String GET_USER = "/get/user/{id}";
-  public static final String UPDATE_USER = "/update/user/{id}";
-  public static final String UPDATE_USER_EMAIL =
-      "/update/user/{id}/{userEmail}";
-  public static final String DELETE_USER = "/delete/user/{id}";
-  public static final String CREATE_USER = "/create/user";
-  public static final String CREATE_USER_BASIC =
-      "/create/user/{userEmail}/{primaryRole}";
-  
-  public static final String GET_SYSTEM_PRODUCTS = "/get/systemProducts";
-  public static final String GET_SYSTEM_PRODUCT = "/get/systemProduct/{id}";
-  public static final String GET_SYSTEM_PRODUCT_SCORES =
-      "/get/systemProduct/scores/{id}";
-  public static final String UPDATE_SYSTEM_PRODUCT =
-      "/update/systemProduct/{id}";
-  public static final String DELETE_SYSTEM_PRODUCT =
-      "/delete/systemProduct/{id}";
-  public static final String CREATE_SYSTEM_PRODUCT =
- "/create/systemProduct";
-
-  public static final String GET_SYSTEM_VULNERABILITIES =
-      "/get/systemVulnerabilities";
-  public static final String GET_SYSTEM_VULNERABILITY =
-      "/get/systemVulnerability/{id}";
-  public static final String GET_SYSTEM_VULNERABILITY_SCORES =
-      "/get/systemVulnerability/scores/{id}";
-  public static final String UPDATE_SYSTEM_VULNERABILITY =
-      "/update/systemVulnerability/{id}";
-  public static final String DELETE_SYSTEM_VULNERABILITY =
-      "/delete/systemVulnerability/{id}";
-  public static final String CREATE_SYSTEM_VULNERABILITY =
-      "/create/systemVulnerability";
-
-  public static final String GET_ROLES = "/get/roles";
-  public static final String GET_ROLE = "/get/role/{id}";
-  public static final String UPDATE_ROLE = "/update/role/{id}";
-  public static final String DELETE_ROLE = "/delete/role/{id}";
-  public static final String CREATE_ROLE = "/create/role";
-
-  public static final String GET_RISK_PREFERENCES = "/get/riskPreferences";
-  public static final String GET_RISK_PREFERENCE = "/get/riskPreference/{id}";
-  public static final String UPDATE_RISK_PREFERENCE =
-      "/update/riskPreference/{id}";
-  public static final String DELETE_RISK_PREFERENCE =
-      "/delete/riskPreference/{id}";
-  public static final String CREATE_RISK_PREFERENCE =
- "/create/riskPreference";
-
-  public static final String GET_PROJECT_SYSTEMS = "/get/projectSystems";
-  public static final String GET_PROJECT_SYSTEM = "/get/projectSystem/{id}";
-  public static final String GET_PROJECT_SYSTEM_SCORES =
-      "/get/projectSystem/scores/{id}";
-  public static final String UPDATE_PROJECT_SYSTEM =
-      "/update/projectSystem/{id}";
-  public static final String DELETE_PROJECT_SYSTEM =
-      "/delete/projectSystem/{id}";
-  public static final String CREATE_PROJECT_SYSTEM =
- "/create/projectSystem";
-
-  public static final String GET_PROJECT_PARTNERS = "/get/projectPartners";
-  public static final String GET_PROJECT_PARTNER = "/get/projectPartner/{id}";
-  public static final String UPDATE_PROJECT_PARTNER =
-      "/update/projectPartner/{id}";
-  public static final String DELETE_PROJECT_PARTNER =
-      "/delete/projectPartner/{id}";
-  public static final String CREATE_PROJECT_PARTNER =
- "/create/projectPartner";
-
-  public static final String GET_PROJECT_DETAILS = "/get/projectDetails";
-  public static final String GET_PROJECT_DETAIL = "/get/projectDetail/{id}";
-  public static final String UPDATE_PROJECT_DETAIL =
-      "/update/projectDetail/{id}";
-  public static final String DELETE_PROJECT_DETAIL =
-      "/delete/projectDetail/{id}";
-  public static final String CREATE_PROJECT_DETAIL =
- "/create/projectDetail";
-
-  public static final String GET_PROJECTS = "/get/projects";
-  public static final String GET_PROJECT = "/get/project/{id}";
-  public static final String GET_PROJECT_SCORES = "/get/project/scores/{id}";
-  public static final String UPDATE_PROJECT = "/update/projects/{id}";
-  public static final String DELETE_PROJECT = "/delete/projects/{id}";
-  public static final String CREATE_PROJECT = "/create/projects";
-
-  public static final String GET_NVD_ENTRY_MESSAGES = "/get/nvdEntryMessages";
-  public static final String GET_NVD_ENTRY_MESSAGE =
-      "/get/nvdEntryMessage/{id}";
-  public static final String GET_NVD_ENTRY_MESSAGE_SCORES =
-      "/get/nvdEntryMessage/scores/{id}";
-  public static final String UPDATE_NVD_ENTRY_MESSAGE =
-      "/update/nvdEntryMessage/{id}";
-  public static final String DELETE_NVD_ENTRY_MESSAGE =
-      "/delete/nvdEntryMessage/{id}";
-  public static final String CREATE_NVD_ENTRY_MESSAGE =
-      "/create/nvdEntryMessage";
-
-  public static final String GET_FLAGGED_ASSETS = "/get/flaggedAssets";
-  public static final String GET_FLAGGED_ASSET = "/get/flaggedAsset/{id}";
-  public static final String UPDATE_FLAGGED_ASSET = "/update/flaggedAsset/{id}";
-  public static final String DELETE_FLAGGED_ASSET = "/delete/flaggedAsset/{id}";
-  public static final String CREATE_FLAGGED_ASSET = "/create/flaggedAsset";
-
-  public static final String LOCALIZATION = "/localization";
-  public static final String OVERVIEW = "/overview";
-  
-  public static final String RSS_NVD_20_ALL = "/rss/nvd/all";
-  public static final String RSS_NVD_CVE_20_2015_URL =
-      "https://nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-2015.xml";
-  public static final String RSS_NVD_CVE_20_2014_URL =
-      "https://nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-2014.xml";
-  public static final String RSS_NVD_CVE_20_2013_URL =
-      "https://nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-2013.xml";
-
-  public static final String RSS_NVD_20_MODIFIED = "/rss/nvd/modified";
-  public static final String RSS_NVD_CVE_20_MODIFIED_URL =
-      "http://nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-Modified.xml";
-
-  public static final String RSS_NVD_20_RECENT = "/rss/nvd/recent";
-  public static final String RSS_NVD_CVE_20_RECENT_URL =
-      "https://nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-Recent.xml";
-
-  // CallableController
-  public static final String ASYNC_CALLABLE = "/async/callable";
-  public static final String ASYNC_VIEW = "/view";
-  public static final String ASYNC_EXCEPTION = "/exception";
-  public static final String ASYNC_CUSTOM_TIMEOUT_HANDLING =
- "/custom-timeout-handling";
-  public static final String ASYNC_RESPONSE_BODY =
- "/response-body";
-
-  // FileUpload Controller
-  public static final String FILE_UPLOAD = "/fileupload";
-  
-  public static final String PROJECTS_HOME = "/projects";
-  public static final String PRODUCTS_HOME = "/products";
-  public static final String RISK_ANALYSIS_HOME = "/riskAnalysis";
-  public static final String GEOSPATIAL_HOME = "/geospatial";
-  public static final String METRICS_HOME = "/metrics";
-
+    public static final String INDEX = "/index";
 }
